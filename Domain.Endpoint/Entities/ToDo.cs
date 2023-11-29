2831c0716c514705795714060ca5dcf3076a3284
judpavon08 <Jpavon202308@gmail.com>
2023-11-28 19:51:23 -0600

    Finalizado listo para entregarse.

---
+++ Domain.Endpoint/Entities/ToDo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Domain.Endpoint.Entities
+{
+    public enum ToDoStatus
+    {
+        InProgress,
+        Completed,
+        NotStarted
+    }
+
+    public class ToDo : BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Done { get; set; }
+        public ToDoStatus Status { get; set; }
+        public Nullable<DateTime> StartedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
